c4ead82aaf47c8046d4e2785ad87d149c2fe5035
Gaven <ghuynh@student.cscc.edu>
2026-03-05 21:07:15 -0500

    Docstring created

---
+++ Lab8_ghuynh-1.py
@@ -0,0 +1,7 @@
+"""
+UPC Validator
+Gaven Huynh
+This program will ask the user for a 12-digit UPC-A code and calculate the correct check digit.
+It should also determine if the UPC is valid.
+March 5, 2026
+"""
